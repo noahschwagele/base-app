6c74db0fd7f9b970a8e036e9043989405d74a529
Noah Schwagele <noahschwagele@gmail.com>
2026-03-15 17:57:00 +0200

    feat: Update notification handling to use expo-device for device checks and adjust orientation in app.json

--- src/contexts/NotificationContext.js
+++ src/contexts/NotificationContext.js
@@ -3,7 +3,7 @@
 import * as Notifications from "expo-notifications";
 import { createContext, useEffect, useState } from "react";
 import { Platform } from "react-native";
-import Constants from "expo-constants";
+import * as Device from "expo-device";
 
 export const NotificationContext = createContext();
 
@@ -12,8 +12,8 @@ export const NotificationProvider = ({ children }) => {
     const [notification, setNotification] = useState(false);
 
     useEffect(() => {
-        registerForExpoPushNotificationsAsync().then((token) =>
-            setExpoPushToken(token)
+        registerForPushNotificationsAsync().then((token) =>
+            setNotification(token)
         );
         console.log("Expo Push Token:", expoPushToken);
     }, []);
@@ -39,7 +39,7 @@ export async function sendLocalNotification(title, body, data = {}) {
 
 async function registerForPushNotificationsAsync() {
     let token;
-    if (Constants.isDevice) {
+    if (Device.isDevice) {
         const { status: existingStatus } = await Notifications.getPermissionsAsync();
         let finalStatus = existingStatus;
         if (existingStatus !== "granted") {
@@ -70,7 +70,7 @@ async function registerForPushNotificationsAsync() {
 //Register for expo token
 async function registerForExpoPushNotificationsAsync() {
     let token;
-    if (Constants.isDevice) {
+    if (Device.isDevice) {
         const { status: existingStatus } = await Notifications.getPermissionsAsync();
         let finalStatus = existingStatus;
         if (existingStatus !== "granted") {
